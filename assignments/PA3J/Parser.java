ff4b9b0a84661f4a13f01d9af7bc179dbbcf64e4
Zanqi <zanqi.liang@gmail.com>
2024-03-10 04:53:30 +0000

    calc cup

--- assignments/PA3J/Parser.java
+++ assignments/PA3J/Parser.java
@@ -26,23 +26,23 @@ import java_cup.runtime.Symbol;
 /** The parser driver class */
 class Parser {
 
-    /** Reads lexed tokens from consosle, and outputs the parse tree */
-    public static void main(String[] args) {
-	args = Flags.handleFlags(args);
-	try {
-	    CoolTokenLexer lexer = new CoolTokenLexer(new InputStreamReader(System.in));
-	    CoolParser parser = new CoolParser(lexer);
-	    Symbol result = (Flags.parser_debug 
-			     ? parser.debug_parse()
-			     : parser.parse());
-	    if (parser.omerrs > 0) {
-		System.err.println("Compilation halted due to lex and parse errors");
-		System.exit(1);
-	    }
-	    ((Program)result.value).dump_with_types(System.out, 0);
-	} catch (Exception ex) {
-	    ex.printStackTrace(System.err);
-	    Utilities.fatalError("Unexpected exception in parser");
+	/** Reads lexed tokens from consosle, and outputs the parse tree */
+	public static void main(String[] args) {
+		args = Flags.handleFlags(args);
+		try {
+			CoolTokenLexer lexer = new CoolTokenLexer(new InputStreamReader(System.in));
+			CoolParser parser = new CoolParser(lexer);
+			Symbol result = (Flags.parser_debug
+					? parser.debug_parse()
+					: parser.parse());
+			if (parser.omerrs > 0) {
+				System.err.println("Compilation halted due to lex and parse errors");
+				System.exit(1);
+			}
+			((Program) result.value).dump_with_types(System.out, 0);
+		} catch (Exception ex) {
+			ex.printStackTrace(System.err);
+			Utilities.fatalError("Unexpected exception in parser");
+		}
 	}
-    }
 }
